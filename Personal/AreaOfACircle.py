e5553f4771d64bee692c924359d55f9f2c6c3ac9
Stephen Stack <stephen.stack@Stephen-Stack.local>
2018-09-11 16:14:49 -0400

    Area of circle calculator

---
+++ Personal/AreaOfACircle.py
@@ -0,0 +1,12 @@
+print ("This is a program that will calculate the surface area of a circle")
+
+#input
+d = input("State Diameter: ")
+d = float(d)
+#process
+a = 3.14*(d/2)*(d/2)
+
+#output
+print ("A circle with diameter "+str(d))
+
+print(" has an area of "+str(a))
