5b99f9f92ea400452706e103c31be306f54e2aa6
Stephen Stack <stephen.stack@Stephen-Stack.local>
2018-11-01 12:02:58 -0400

    Developing Ideas strings

---
+++ Contest Questions/CCC2018J1SS.py
@@ -0,0 +1,1 @@
+print("CCC2018J1SS")
---
+++ DevelopingIdeasStrings.py
@@ -0,0 +1,13 @@
+#INPUT
+
+#Step 1 take input for first name
+fName = input("Input First Name")
+#Step 2 take input for last name
+lName = input("Input Last Name")
+
+#Process 
+
+userName = fName[0] + lName
+
+#Output
+print(userName)
